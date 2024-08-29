1417f82cd46a67b50b3d06f27db82c6af9b01fe8
WhiteSnake <whitesnake35711@gmail.com>
2024-08-29 22:13:41 +0530

    initial commit

---
+++ client/src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+
+import {Profile, Home, Login, Register} from './pages'
+
+const Layout = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<App />}>
+        <Route path='' element={<Home />} />
+        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Register />} />
+        <Route path='profile' element={<Profile />} />
+      </Route>
+    )
+  )
+  return (
+      <RouterProvider router={router} />
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Layout />
+  </React.StrictMode>,
+)
